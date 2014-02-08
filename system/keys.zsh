7a21b4343b032033154fa7b5c9218a50a3d87d0b
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-02-08 12:56:00 +0100

    changed my pub key

--- system/keys.zsh
+++ system/keys.zsh
@@ -7,4 +7,4 @@
 ####
 
 # Pipe my public key to my clipboard. Fuck you, pay me.
-alias pubkey="more ~/.ssh/id_dsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
