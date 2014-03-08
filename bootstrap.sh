4c9c6c9c34cb2a02c15ca8c19ddcb59fc1d4d576
Sebastian Ruml <sebastian.ruml@netlight.com>
2014-03-08 22:53:18 +0100

    added bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,18 @@
-# TODO: Fill with some content!
+#!/bin/sh
 
 ####
-# Bootstrap script for the dotfiles. This script performs the first time
-# installation of the dotfiles on a new machine. It installs e.g. required
-# tools and applications.
+# Bootstrap script for dotfiles. This script performs the first time
+# installation of dotfiles on a new machine.
 #
 # To manage your dotfile repo use rake and make changes to the Rakefile if
 # necessary.
 ####
 
-
-# TODO
+if [ ! -d "$HOME/.dotfiles" ]; then
+    echo "Installing dotfiles for the first time..."
+    git clone https://github.com/hypebeast/dotfiles.git "$HOME/.dotfiles"
+    cd "$HOME/.dotfiles"
+    rake install
+else
+    echo "dotfiles is already installed"
+fi
