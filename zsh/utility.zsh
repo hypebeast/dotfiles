8034362841cb9fae9e075c6119cec454209d2ce4
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-08-03 16:28:43 +0200

    some more ZSH aliases and functions

---
+++ zsh/utility.zsh
@@ -0,0 +1,6 @@
+# File Download
+if (( $+commands[curl] )); then
+    alias get='curl --continue-at - --location --progress-bar --remote-name --remote-time'
+elif (( $+commands[wget] )); then
+    alias get='wget --continue --progress=bar --timestamping'
+fi
