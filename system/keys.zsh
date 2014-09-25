3e3341c9e7834a48c6d828cda502ccfa8bc16d8a
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-09-25 11:03:46 +0200

    add function to copy my public key to a remote host and add it to the authorized keys

--- system/keys.zsh
+++ system/keys.zsh
@@ -8,3 +8,8 @@
 
 # Pipe my public key to my clipboard. Fuck you, pay me.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+# Copy my public key to a remote host
+function ssh-copy () {
+    cat ~/.ssh/id_rsa.pub | ssh $@ 'mkdir -p ~/.ssh; cat >> ~/.ssh/authorized_keys'
+}
