db03a27a6bdc09a38db1a51f840828318a2e85ab
Sebastian Ruml <sebastian.ruml@gmail.com>
2016-09-14 22:43:41 +0200

    PATH cleanup

--- system/path.zsh
+++ system/path.zsh
@@ -6,7 +6,7 @@
 SCALA_HOME=$HOME/bin/scala-2.11.7
 
 # Path
-PATH=/usr/local/Trolltech/Qt-4.8/bin:/Users/sruml/bin:$SCALA_HOME/bin:/Users/sruml/.dotfiles/bin:/usr/local/CrossPack-AVR/bin:/Users/sruml/Apps/yagarto-4.7.2/bin:$HOME/yagarto/yagarto-4.7.2/tools:$PATH
+PATH=$HOME/bin:$SCALA_HOME/bin:$HOME/.dotfiles/bin:$PATH
 
 # Pythonpath
 export PYTHONPATH=/usr/local/lib/python:$PYTHONPATH
@@ -17,18 +17,21 @@ FLIP_PATH=/Users/sruml/Apps/flip.3.2.1/bin
 PATH=$FLIP_PATH:$PATH
 
 # Anaconda
-PATH=~/anaconda/bin:$PATH
+PATH=$HOME/anaconda/bin:$PATH
 
 # QT
-PATH=~/Apps/Qt/5.1.1/clang_64/bin/:$PATH
+PATH=$HOME/Apps/Qt/5.1.1/clang_64/bin/:$PATH
 
 # Activator
 PATH=$HOME/bin/activator/:$PATH
 
 # Gradle
-PATH=~/bin/gradle-2.4/bin:$PATH
+PATH=$HOME/bin/gradle-2.4/bin:$PATH
 
 # Pipsi
-PATH=~/.local/venvs/pipsi/bin:$PATH
+PATH=$HOME/.local/venvs/pipsi/bin:$PATH
+
+# Python/Pip
+PATH=$HOME/.local/bin:$PATH
 
 export PATH
