48b67068fc5fcb3a3bdbf7a858c9e1b3869132c7
Sebastian Ruml <sebastian.ruml@gmail.com>
2017-01-11 13:42:37 +0100

    add brew package git-extras

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -94,6 +94,7 @@ brew install reattach-to-user-namespace
 brew install typesafe-activator
 brew install rbenv
 brew install golang
+brew install git-extras
 
 # Remove outdated versions from the cellar.
 brew cleanup
