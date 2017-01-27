1478d1010d71447cf9d57f51433ff9e623fe4b22
Sebastian Ruml <sebastian.ruml@gmail.com>
2017-01-27 17:23:52 +0100

    add checkout branches function for fzf

--- fzf/.oh-my-zsh/custom/fzf.zsh
+++ fzf/.oh-my-zsh/custom/fzf.zsh
@@ -31,3 +31,12 @@ fkill() {
   fi
 }
 
+# fbr - checkout git  ranch (including remote branches)
+fbr() {
+  local branches branch
+  branches=$(git branch --all | grep -v HEAD) &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
