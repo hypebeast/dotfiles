6135889b6fd9f1dd299346ac0849b543cbbb7417
Sebastian Ruml <sebastian.ruml@gmail.com>
2017-01-28 22:34:37 +0100

    add siji font

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Install fonts for powerline from https://github.com/powerline/fonts
+# Install various fonts
 #
 
 if [ ! -d ~/.dotfiles/libs ]; then
@@ -9,14 +9,30 @@ fi
 
 cd ~/.dotfiles/libs
 
+# Install fonts for powerline from https://github.com/powerline/fonts
 if [ ! -d "./powerline-fonts" ]; then
 	git clone "https://github.com/powerline/fonts" "./powerline-fonts"
-  cd "powerline-fonts"
+    cd "powerline-fonts"
 else
 	cd "powerline-fonts"
 	git pull origin master
 fi
 
 ./install.sh
+
+if [[ $(uname) == "Linux" ]]; then
+    cd ~/.dotfiles/libs
+
+    # Install siji fonts from https://github.com/stark/siji.git
+    if [ ! -d "./siji" ]; then
+        git clone "https://github.com/stark/siji.git" "./siji"
+        cd "siji"
+    else
+        cd "siji"
+        git pull origin master
+    fi
+
+    ./install.sh
+fi
 
 exit 0
