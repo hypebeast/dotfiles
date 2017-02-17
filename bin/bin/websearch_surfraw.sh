2ca376992316509f86121d25da20776954c35def
Sebastian Ruml <sebastian.ruml@gmail.com>
2017-02-17 18:22:04 +0100

    rofi scripts

---
+++ bin/bin/websearch_surfraw.sh
@@ -0,0 +1,1 @@
+surfraw $(surfraw -elvi | awk -F'-' '{print $1}' | sed '/:/d' | awk '{$1=$1};1' | rofi -padding 400 -fullscreen -kb-row-select "Tab" -kb-row-tab "Control+space" -dmenu -mesg ">>> Tab = Autocomplete" -i -p "websearch: ")
