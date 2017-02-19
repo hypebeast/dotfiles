2253d1b21b7035bf95ef8cc2e58973c9408531ea
Sebastian Ruml <sebastian.ruml@gmail.com>
2017-02-19 23:03:08 +0100

    add default search engine

--- bin/bin/websearch_surfraw.sh
+++ bin/bin/websearch_surfraw.sh
@@ -1,1 +1,27 @@
-surfraw $(surfraw -elvi | awk -F'-' '{print $1}' | sed '/:/d' | awk '{$1=$1};1' | rofi -padding 400 -fullscreen -kb-row-select "Tab" -kb-row-tab "Control+space" -dmenu -mesg ">>> Tab = Autocomplete" -i -p "websearch: ")
+#!/bin/bash
+
+default_search_engine="google"
+help_color="#0C73C2"
+
+list_search_engines=$(surfraw -elvi | awk '{if (NR != 1) print "?"$1}')
+
+
+function main() {
+	HELP_MSG="<span color=\"$help_color\">Hit Tab to complete search engine name
+Searches without prepended engine use "$default_search_engine"</span>"
+	elvi=$(echo -e "${list_search_engines}" | rofi -padding 400 -fullscreen -kb-row-select "Tab" -kb-row-tab "Control+space" -dmenu -mesg "${HELP_MSG}" -i -p "websearch: ")
+
+	if [[ $elvi == "" ]]; then exit 1
+	elif [[ $elvi == "?"* ]]; then
+		name=$(echo "${elvi}" | awk '{ print $1 }' | cut -c 2-)
+		search=$(echo "${elvi}" | awk '{ $1=""; print $0 }' | cut -c 2-)
+		surfraw ${name} ${search}
+	else
+		name="${default_search_engine}"
+		search="${elvi}"
+
+		surfraw ${name} ${search}
+	fi
+}
+
+main
