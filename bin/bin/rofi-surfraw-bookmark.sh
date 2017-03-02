dba1b5c9394019b266e787f42fc93bb64c1e8224
Sebastian Ruml <sebastian.ruml@gmail.com>
2017-03-02 22:26:03 +0100

    update rofi bookmark manager

--- bin/bin/rofi-surfraw-bookmark.sh
+++ bin/bin/rofi-surfraw-bookmark.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 HELP_MSG=">>> Edit to add new bookmarks at ~/.config/surfraw/bookmarks"
-LAUNCHER="rofi -fullscreen -padding 200 -dmenu -mesg $HELP_MSG -columns 1 -i -p 'bookmarks: '"
+LAUNCHER="rofi -fullscreen -padding 200 -dmenu -mesg $HELP_MSG -columns 1 -i -p bookmarks: "
 BOOKMARKS_FILE=~/.config/surfraw/bookmarks
 
 cleaned_bookmarks=$(cat ${BOOKMARKS_FILE} | sed '/^$/d' | sed '/^#/d' | sed '/^\//d')
 
-bookmarks=$(echo "$cleaned_bookmarks" | awk -F' ' '{$1=substr($1, 0, 49); $1=sprintf("%-55s", $1);print}' | sort)
+bookmarks=$(echo -e "$cleaned_bookmarks" | awk -F' ' '{$1=substr($1, 0, 49); $1=sprintf("%-55s", $1);print}' | sort)
 
 surfraw "$(echo -e "${bookmarks}" | ${LAUNCHER} | awk -F' ' '{print $2}')"
