674e5482c107d525dd3b24eaa920fb873989d6ee
Sebastian Ruml <sebastian.ruml@gmail.com>
2017-03-30 14:59:55 +0200

    increase memory limit to 2G for java

--- zsh/.oh-my-zsh/custom/java.zsh
+++ zsh/.oh-my-zsh/custom/java.zsh
@@ -2,4 +2,4 @@
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home/
 
 # Java Opts
-export JAVA_OPTS="-Xms128M -Xmx512M -XX:MaxPermSize=512M"
+export JAVA_OPTS="-Xms128M -Xmx2G"
