c89c0aaafe0c8a19b6e0246398186984f71cc897
Sebastian Ruml <sebastian.ruml@gmail.com>
2017-12-17 19:00:57 +0100

    add new command

---
+++ cnotes/.oh-my-zsh/custom/cnotes.zsh
@@ -0,0 +1,37 @@
+###########################################################
+### cnotes
+###
+### Command line tool to work with a git based wiki.
+###########################################################
+
+CNOTES_WIKI_DIR=${CNOTES_WIKI_DIR:-}
+CNOTES_EDITOR=${EDITOR:-vim}
+
+# cnotes-new creates a new note and saves it.
+function cnotes-new() {
+    local cmd="command find -L ${CNOTES_WIKI_DIR} -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+        -o -type d -print 2> /dev/null"
+
+	local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" fzf-tmux +m)"
+
+	echo "Selected folder \"${dir}\"\n"
+
+	read "file?Enter file name for new note: "
+
+	file=${dir}/${file}
+
+	if [[ -z ${file} ]]
+	then
+		touch ${file}
+	fi
+
+	${CNOTES_EDITOR} "${file}"
+}
+
+function cnotes-open() {
+
+}
+
+function cnotes-search() {
+
+}
