8bc0403a3950cff9daa58dd3cb4ace40ab4fb765
Sebastian Ruml <sebastian.ruml@gmail.com>
2017-12-18 09:23:48 +0100

    code cleanup

--- fzf/.oh-my-zsh/custom/fzf.zsh
+++ fzf/.oh-my-zsh/custom/fzf.zsh
@@ -136,8 +136,8 @@ alias fbk=fzf-surfraw
 
 # jump to resently used directory
 j() {
-    [ $# -gt 0  ] && fasd_cd -d "$*" && return
     local dir
+    [ $# -gt 0  ] && fasd_cd -d "$*" && return
     dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
 }
 
