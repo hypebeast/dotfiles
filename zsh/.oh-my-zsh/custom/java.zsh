0861c95631048c3a2107f387f5c92610630a6dd8
Sebastian Ruml <sebastian.ruml@gmail.com>
2018-03-17 09:51:19 +0100

    alias for mc and new JAVA_HOME

--- zsh/.oh-my-zsh/custom/java.zsh
+++ zsh/.oh-my-zsh/custom/java.zsh
@@ -1,5 +1,5 @@
 # Java Home
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_112.jdk/Contents/Home/
 
 # Java Opts
 export JAVA_OPTS="-Xms128M -Xmx2G"
