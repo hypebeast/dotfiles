1273afe9ad37cd76a43fb91f2f44ccf1617ee0d4
Sebastian Ruml <sebastian.ruml@gmail.com>
2018-10-07 10:35:14 +0200

    check for rg and not ag

--- fzf/.oh-my-zsh/custom/fzf.zsh
+++ fzf/.oh-my-zsh/custom/fzf.zsh
@@ -7,9 +7,9 @@
 # Load fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-# fzf + ag configuration
-if _has fzf && _has ag; then
-  export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*,vendor" 2> /dev/null'
+# fzf + rg configuration
+if _has fzf && _has rg; then
+  export FZF_DEFAULT_COMMAND="rg --files --no-ignore --hidden --follow -g '!{.git,node_modules}/*,vendor' 2> /dev/null"
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
   export FZF_ALT_C_COMMAND="bfs -type d -nohidden"
   export FZF_DEFAULT_OPTS="--color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108,info:108,prompt:109,spinner:108,pointer:168,marker:168"
