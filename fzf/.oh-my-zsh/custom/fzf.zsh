14f90827b9692fe58b53a92e8678422a6b89d2a7
Sebastian Ruml <sebastian.ruml@gmail.com>
2021-12-06 12:58:49 +0100

    more updates

--- fzf/.oh-my-zsh/custom/fzf.zsh
+++ fzf/.oh-my-zsh/custom/fzf.zsh
@@ -167,5 +167,3 @@ z() {
     local dir
     dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
 }
-alias j=z
-
--- zsh/.oh-my-zsh/custom/functions.zsh
+++ zsh/.oh-my-zsh/custom/functions.zsh
@@ -123,3 +123,23 @@ alias opn=open-notes
 function status-code () {
     curl -s https://httpstatuses.com/$1 | lynx -stdin
 }
+
+function cdd() {
+  cd "$(ls -d -- */ | fzf)" || echo "Invalid directory"
+}
+
+function j() {
+  fname=$(declare -f -F _z)
+
+  [ -n "$fname" ] || source "$DOTFILES_PATH/modules/z/z.sh"
+
+  _z "$1"
+}
+
+function recent_dirs() {
+  # This script depends on pushd. It works better with autopush enabled in ZSH
+  escaped_home=$(echo $HOME | sed 's/\//\\\//g')
+  selected=$(dirs -p | sort -u | fzf)
+
+  cd "$(echo "$selected" | sed "s/\~/$escaped_home/")" || echo "Invalid directory"
+}
