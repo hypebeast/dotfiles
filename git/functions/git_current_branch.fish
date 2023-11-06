efe67def0d8df1e09c4ca8bc3ece1ab5ad3a14d6
Sebastian Ruml <sebastian@sebastianruml.name>
2023-11-06 15:31:43 +0100

    fix git_current_branch function
    Signed-off-by: Sebastian Ruml <sebastian@sebastianruml.name>

--- git/functions/git_current_branch.fish
+++ git/functions/git_current_branch.fish
@@ -1,3 +1,3 @@
 function git_current_branch -d "Shows the current Git branch"
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+    git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 end
