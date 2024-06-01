d89165521e7c3fcdd87b77d23a28770a1788538d
Sebastian Ruml <sebastian@sebastianruml.name>
2024-06-01 18:28:52 +0200

    set GOPATH env var
    Signed-off-by: Sebastian Ruml <sebastian@sebastianruml.name>

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,26 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# PATH
-fish_add_path -a $DOTFILES/bin $HOME/.bin $HOME/.local/bin /opt/homebrew/bin /usr/local/bin /opt/homebrew/opt/libpq/bin
+# Set PATH
+fish_add_path -a $DOTFILES/bin $HOME/.bin $HOME/.local/bin /opt/homebrew/bin /usr/local/bin /opt/homebrew/opt/libpq/bin $HOME/.pub-cache/bin $PYENV_ROOT/bin $HOME/.dotnet/tools $HOME/Coding/go/bin
 
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+# Pyenv
+set -Ux PYENV_ROOT $HOME/.pyenv
+pyenv init - | source
+
+# Java
+set -Ux INTEL_HAXM_HOME /usr/local/Caskroom/intel-haxm
+set -Ux JAVA_HOME (/usr/libexec/java_home -v 1.8)
+
+# Flutter
+export FLUTTER_ROOT="$(asdf where flutter)"
+
+# Gopath
+set -Ux GOPATH $HOME/Coding/go
+
+# Direnv
+direnv hook fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/Library/Application Support/reflex/bun"
+set --export PATH $BUN_INSTALL/bin $PATH
