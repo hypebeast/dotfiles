b7475dff1916b63f2a5bbf476aae46f428e1cf34
Sebastian Ruml <sebastian@sebastianruml.name>
2025-03-18 14:57:54 +0100

    update tree commands

--- system/conf.d/system.fish
+++ system/conf.d/system.fish
@@ -5,14 +5,23 @@ function fish_greeting
 end
 funcsave -q fish_greeting
 
-abbr -a less 'less -r'
+if command -qs eza
+    alias l='eza -lah --icons'
+    alias ll='eza -lh --icons'
+    alias lr='eza -lhr --icons'
+    alias lr='eza -a -l -s changed --icons'
 
-if command -qs exa
-    alias l='exa -lah --icons'
-    alias ll='exa -lh --icons'
-    alias lr='exa -lhr --icons'
-    alias lt='exa -l --icons --tree --level=2'
-    alias lt='exa -l --icons --tree --level=2'
+    abbr -a lt 'eza --tree --level 2 -D'
+    abbr -a ltt 'eza --tree --level 3 -D'
+    abbr -a lttt 'eza --tree --level 4 -D'
+    abbr -a lt3 'eza --tree -D --level 3'
+    abbr -a lt4 'eza --tree -D --level 4'
+    abbr -a lt5 'eza --tree -D --level 5'
+
+    abbr -a t2 'eza --tree --icons --level 2'
+    abbr -a t3 'eza --tree --icons --level 3'
+    abbr -a t4 'eza --tree --icons --level 4'
+    abbr -a t5 'eza --tree --icons --level 5'
 else
     alias l='ls -lAh'
     alias ll='ls -l'
@@ -24,11 +33,6 @@ end
 abbr --add dotdot --regex '^\.\.+$' --function multicd
 
 #abbr -a t1 'tree -I "node_modules|vendor" -d -L 1'
-abbr -a t1 'exa --tree --level 1'
-abbr -a t2 'exa --tree --level 2'
-abbr -a t3 'exa --tree --level 3'
-abbr -a t4 'exa --tree --level 4'
-abbr -a t5 'exa --tree --level 5'
 
 # quickly jump to recently used directories
 abbr -a 1 'prevd 1'
@@ -37,5 +41,10 @@ abbr -a 3 'prevd 3'
 abbr -a 4 'prevd 4'
 abbr -a 5 'prevd 5'
 
+abbr -a less 'less -r'
+abbr -a lg lazygit
+abbr -a repos 'cd ~/Coding'
+abbr -a dotfiles 'cd ~/.dotfiles'
 abbr -a mk 'mkdir -p'
-
+abbr -a v nvim
+abbr -a rg 'rg -i'
